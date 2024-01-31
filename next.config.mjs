5fb17a401c8e2876c018bd08492ae035972fadaa
Yat Fu Chan <chanyatfu0616@gmail.com>
2024-01-31 18:14:13 +0800

    added Nextra setup and basic concept

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import createNextra from 'nextra';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+const withNextra = createNextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+})
+
+export default withNextra(nextConfig);
---
+++ theme.config.jsx
@@ -0,0 +1,6 @@
+export default {
+  logo: <b>demistify.react</b>,
+  project: {
+      link: 'https://github.com/shuding/nextra'
+  }
+}
